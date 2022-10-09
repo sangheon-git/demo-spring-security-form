7d2d3332f4223a22ff61d9653fb55628c4ab5237
sangheon <tkdgjs0415@gmail.com>
2022-10-10 01:28:10 +0900

    2022-10-10 spring-security 6강

---
+++ src/main/java/com/example/demospringsecurityform/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.example.demospringsecurityform.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .mvcMatchers("/","/info").permitAll()
+                .mvcMatchers("/admin").hasRole("ADMIN")
+                .anyRequest().authenticated();
+        http.formLogin();
+        http.httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("sangheon").password("{noop}123").roles("USER").and() //암호화를 해서 DB에 들어가는 값 *noop : 암호화 X
+                .withUser("admin").password("{noop}!@#").roles("ADMIN");
+    }
+
+}
