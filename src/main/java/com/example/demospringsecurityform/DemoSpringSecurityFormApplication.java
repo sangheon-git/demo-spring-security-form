b4c24f4eba1702b38ad52fbe4ec7a81726e818e4
tkdgjs0415 <tkdgjs0415@gmail.com>
2022-10-10 13:41:00 +0900

    2022-10-10 Spring-Security PasswordEncoder 8강

--- src/main/java/com/example/demospringsecurityform/DemoSpringSecurityFormApplication.java
+++ src/main/java/com/example/demospringsecurityform/DemoSpringSecurityFormApplication.java
@@ -2,9 +2,18 @@ package com.example.demospringsecurityform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class DemoSpringSecurityFormApplication {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        // 스피링 시큐리티 5에서 권장하고 있는 인코딩 방식 - 다양한 패스워드 인코딩 지원
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoSpringSecurityFormApplication.class, args);
--- src/main/java/com/example/demospringsecurityform/account/Account.java
+++ src/main/java/com/example/demospringsecurityform/account/Account.java
@@ -1,6 +1,8 @@
 package com.example.demospringsecurityform.account;
 
 
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -51,7 +53,7 @@ public class Account {
     private String role;
 
 
-    public void encodePassword() {
-        this.password = "{noop}" + this.password;
+    public void encodePassword(PasswordEncoder passwordEncoder) {
+        this.password = passwordEncoder.encode(this.password);
     }
 }
